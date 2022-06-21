dd1a0d7fc9fd9ebeec2439548f506b31dc3f36e9
Shugga939 <Shugga939@email.com>
2022-06-21 17:43:16 +0300

    task_2 completed

--- Part_1/Сhapter_2/task_2.js
+++ Part_1/Сhapter_2/task_2.js
@@ -1,1 +1,12 @@
-alert('Я JavaScript!')
+let admin , name 
+name = "Джон"
+admin = name
+alert(admin)
+
+//---
+let ourPlanet
+let currentUserName
+
+//---
+const BIRTHDAY = '18.04.1982'; // можно
+const AGE = someCode(BIRTHDAY); // нельзя
