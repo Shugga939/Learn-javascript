c06d9ad5c4a8cc23d0d5f3e74587b9699518a6fb
Shugga939 <Shugga939@gmail.com>
2022-06-23 19:31:14 +0300

    add task_9

---
+++ Part_1/Сhapter_2/task_9.js
@@ -0,0 +1,47 @@
+let i = 3;
+
+while (i) {
+  alert( i-- );
+}   // 0
+
+//-----
+let i = 0;
+while (++i < 5) alert( i ); // 4
+while (i++ < 5) alert( i ); // 5
+
+//-----
+for (let i = 0; i < 5; i++) alert( i ); //4
+for (let i = 0; i < 5; ++i) alert( i ); //4
+
+//-----
+for (let i = 0; i < 11; ++i) {
+  if (i%2==0) alert( i )
+}
+
+//-----
+let i = 0;
+while(i < 3) {
+  alert( `number ${i}!` )
+  i++
+} 
+
+//-----
+let num;
+
+do {
+  num = prompt("Введите число больше 100?", 0)
+} while (num <= 100 && num)
+
+//-----
+
+let n = 10
+
+nextPrime:
+for (let i = 2; i <= n; i++) { 
+
+  for (let j = 2; j < i; j++) { 
+    if (i % j == 0) continue nextPrime
+  }
+
+  alert( i )
+}
